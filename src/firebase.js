367c788a1f2aa20e4ca8b4eb56b0145bb849bb4e
Aishik Chakraborty <76745298+SLashing447@users.noreply.github.com>
2021-11-16 18:58:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDpB2qE2d38eNdThwya3VO0enTOQx-X-Po",
-  authDomain: "lemon-1c4c5.firebaseapp.com",
-  projectId: "lemon-1c4c5",
-  storageBucket: "lemon-1c4c5.appspot.com",
-  messagingSenderId: "155068374743",
-  appId: "1:155068374743:web:53afb43f967a382f7f5a92",
-  measurementId: "G-D58K92DFGD",
+  // Your Config
 });
 
 const db = firebase.firestore();
